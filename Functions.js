2c81d270a6c72eacfe8dc9009141df7d9c851304
JabinJohnson <133173143+JabinJohnson@users.noreply.github.com>
2023-06-18 18:03:13 -0400

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,21 @@
+function firstLetterName(){
+    alert("The name Jabin Johnson starts with the letter [J]" );
+}
+function divisibleByTwo(x){
+    console.log; if (x % 2 === 0) {
+        return "true";
+      } else {
+        return "false";
+      }
+}
+
+let arr = [3,509,845,7,59,30];
+function largestNum() {
+    for (let i = 1; i < arr.length; i++) {
+       if (arr[i] > largest)
+       largest = arr[i];
+    }
+}
+firstLetterName();
+divisibleByTwo();
+largestNum();
